dd672170e382a80f8290710ec3614ee5cd61b9d0
efrat5454 <efrat5454@gmail.com>
2022-06-21 18:53:38 +0300

    register

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,13 +7,30 @@ const Login = () => {
   const { setUser } = useContext(UserContext);
   const [userEmail, setUserEmail] = useState();
   const [userPass, setUserPass] = useState();
-  const checkUser = (e) => {
+
+  const [userNameRegister, setUserNameRegister] = useState();
+  const [userEmailRegister, setUserEmailRegister] = useState();
+  const [userPassRegister, setUserPassRegister] = useState();
+
+  const login = (e) => {
+    console.log("login");
     e.preventDefault();
     console.log(userEmail);
     console.log(userPass);
+    if (!userEmail) {
+    }
+    if (!userPass) {
+    }
     if (userEmail === userDb.email && userPass === userDb.pass) {
       setUser(userDb);
     }
+  };
+  const register = (e) => {
+    e.preventDefault();
+    console.log("register");
+    console.log(userNameRegister);
+    console.log(userEmailRegister);
+    console.log(userPassRegister);
   };
   return (
     <div id="login">
@@ -21,7 +38,7 @@ const Login = () => {
         <input type="checkbox" id="chk" aria-hidden="true" />
 
         <div className="signup">
-          <form>
+          <form onSubmit={register}>
             <label htmlFor="chk" aria-hidden="true">
               Sign up
             </label>
@@ -30,25 +47,37 @@ const Login = () => {
               name="txt"
               placeholder="User name"
               className="inputLogin"
+              value={userNameRegister}
+              onChange={(e) => {
+                setUserNameRegister(e.target.value);
+              }}
             />
             <input
               type="email"
               name="email"
               placeholder="Email"
               className="inputLogin"
+              value={userEmailRegister}
+              onChange={(e) => {
+                setUserEmailRegister(e.target.value);
+              }}
             />
             <input
               type="password"
               name="pswd"
               placeholder="Password"
               className="inputLogin"
+              value={userPassRegister}
+              onChange={(e) => {
+                setUserPassRegister(e.target.value);
+              }}
             />
             <button className="buttonLogin">Sign up</button>
           </form>
         </div>
 
         <div className="login">
-          <form onSubmit={checkUser}>
+          <form onSubmit={login}>
             <label htmlFor="chk" aria-hidden="true">
               Login
             </label>
